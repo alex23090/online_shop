e8b1825bb6e8bf100da7cef21b433af1024dccc6
alex23090 <alex2003199604@gmail.com>
2022-08-31 18:46:44 +0300

    first commit

---
+++ core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('chechout-page/', views.CheckoutView.as_view(), name='checkout-page'),
+    path('order-summary/', views.OrderSummaryView.as_view(), name='order-summary'),
+    path('product/<slug>/', views.ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', views.remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', views.remove_single_item_from_cart, name='remove-single-item-from-cart'),
+    path('payment/<payment_option>/', views.PaymentView.as_view(), name='payment'),
+    path('add-coupon/', views.AddCouponView.as_view(), name='add-coupon'),
+    path('request-refund/', views.RequestRefundView.as_view(), name='request-refund'),
+]
